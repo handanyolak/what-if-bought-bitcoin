36d9c60e0b8fd58068036bfe78340135f1dbb600
Handan Yolak <handanyolak@gmail.com>
2022-01-23 17:54:30 +0300

    initialize

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <div class="bg-dark-gray">
+      <Nuxt />
+    </div>
+  </v-app>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen main-page">index.vue</div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
+
