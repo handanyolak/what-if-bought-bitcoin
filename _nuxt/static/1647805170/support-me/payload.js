17b26f017ca4201b5bf39e07eba6a91fd81d23fd
handanyolak <handanyolak@users.noreply.github.com>
2022-03-20 19:39:31 +0000

    deploy: 9b3496360fb28973d5a429fb47cd47b76cdad225

---
+++ _nuxt/static/1647805170/support-me/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/support-me", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1647805170/support-me/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647805170",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsupport-me",config:{ownerAddress:"0xb91760bA38F185660755fEEcDFaeCe974Ac04A91",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
